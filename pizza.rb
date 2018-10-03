eb4be4ccb47572c6d4c2f9fe9a073c7e65fb855d
bbarker63 <betterlivinghms@gmail.com>
2018-10-03 15:39:43 -0400

    finally got this working. jesus

--- pizza.rb
+++ pizza.rb
@@ -6,7 +6,7 @@ def pizza_price()
 end
 
 def pizza_size()
-	["small", "medium", "large"].sample
+	size = ["small", "medium", "large"].sample
 end
 
 def pizza_crust()
@@ -19,8 +19,10 @@ end
 
 def pizza_veggie()
 	veggie = ["banana peppers", "olives", "mushroom", "onion"].sample
+end
 	def pizza_extra()
 		extra = ["extra cheese", "extra sauce", "double pepperoni"].sample
+end
 
 def topping(x)
 loop do
@@ -36,14 +38,14 @@ loop do
 		break
 	end
 end
-
+end
 
 	def total(num1, num2, num3, num4)
 		p (num1 * num2 + num3 + num4)
 	end
-	count = 1
 	size_price = 0
 	delivery_cost = 0
+	extra_cost = 0
 	puts "any extra toppings yes or no?"
 	x = gets.chomp
 	topping(x)
@@ -51,32 +53,52 @@ end
 	puts "delivery?"
 	d = gets.chomp
 
-	pizza.times do
-		pizza_size = size
-		if size == "small"
-			size_price += 5
+pizza.times do
+	x = pizza_size
+	if x == "small"
+		size_price += 5
+	elsif x == "medium"
+		size_price += 10
+	else
+		size_price += 15
+	end
+	# size = size_price
+		#if pizza_size == "small"
+			#size_price += 5
+		#elsif pizza_size == "medium"
+			#size_price += 10
+		#else
+			#size_price += 15
+		#end
 
-		elsif size == "medium"
-			size_price += 10
+		
 
-		elsif size = "large"
-			size_price += 15
-
-		end
-
+		
 		if d == "yes"
 			delivery_cost = 2
 		else d == "no"
 			delivery_cost = 0
 		end
-		put
 
+		# if x == "yes"
+		# 	extra_cost = 2
+		# else x == "no"
+		# 	extra_cost = 0
+		# end
 	
-puts "#{count}: You've ordered a #{pizza_size} #{pizza_crust}, with #{pizza_meat}, #{pizza_veggie}, and extra #{pizza_extra}"
+
+
+
+puts "You've ordered a #{x} #{pizza_crust}, with #{pizza_meat}, #{pizza_veggie}, and extra #{pizza_extra}"
 	
+end
+
+puts "Your total is" 
+total(pizza_price, pizza, size_price, delivery_cost)
 
 	puts "would you like to place another order?"
 	order = gets.chomp
 	if order == "yes" then load 'pizza.rb'
 	else exit
 	end
+
